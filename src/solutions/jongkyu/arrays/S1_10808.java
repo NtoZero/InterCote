3663372e743bfac809fbd7752f4e05acd265896e
LST <dev.st1027@gmail.com>
2023-11-05 14:21:26 +0900

    :sparkles: feat: 예시 문제 추가

---
+++ src/solutions/jongkyu/arrays/S1_10808.java
@@ -0,0 +1,6 @@
+package solutions.jongkyu.arrays;
+
+import problems.arrays.P1_10808;
+
+public class S1_10808 implements P1_10808 {
+}
---
+++ src/solutions/yeji/arrays/S1_10808.java
@@ -0,0 +1,6 @@
+package solutions.yeji.arrays;
+
+import problems.arrays.P1_10808;
+
+public class S1_10808 implements P1_10808 {
+}
